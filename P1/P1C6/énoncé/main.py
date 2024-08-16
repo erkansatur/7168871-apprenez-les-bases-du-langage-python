1ea0cd2a273e12a1ac867ada6c94df4ba25cb175
erkansatur <ericsatur81@gmail.com>
2024-08-16 14:47:04 +0200

    main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,14 @@
 # Écrivez votre code ici !
+fruit = ["pomme","banane","orange"]
+print (fruit)
+fruit.append("kiwi")
+print (fruit)
+fruit.remove("orange")
+print(fruit)
+len(fruit)
+fruit.sort()
+
+
+
+
+
